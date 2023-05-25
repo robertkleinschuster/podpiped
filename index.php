cc8cc198682ff989c01ad11d49953d4747f43fd0
robertkleinschuster <github@kleinschuster.de>
2023-05-25 14:33:53 +0200

    thumbnails: fallback to hqdefault image it maxresdefault is not available

--- index.php
+++ index.php
@@ -2,8 +2,316 @@
 
 declare(strict_types=1);
 
+date_default_timezone_set('UTC');
+
+class Channel
+{
+    private string $title = '';
+    private string $frontend = '';
+    private string $language = '';
+    private string $description = '';
+    private string $feedUrl = '';
+    private string $copyright = '';
+    private string $cover = '';
+    private string $items = '';
+
+    /**
+     * @param string $title
+     * @return Channel
+     */
+    public function setTitle(string $title): Channel
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @param string $frontend
+     * @return Channel
+     */
+    public function setFrontend(string $frontend): Channel
+    {
+        $this->frontend = htmlentities($frontend);
+        return $this;
+    }
+
+    /**
+     * @param string $language
+     * @return Channel
+     */
+    public function setLanguage(string $language): Channel
+    {
+        $this->language = $language;
+        return $this;
+    }
+
+    /**
+     * @param string $description
+     * @return Channel
+     */
+    public function setDescription(string $description): Channel
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @param string $feedUrl
+     * @return Channel
+     */
+    public function setFeedUrl(string $feedUrl): Channel
+    {
+        $this->feedUrl = $feedUrl;
+        return $this;
+    }
+
+    /**
+     * @param string $copyright
+     * @return Channel
+     */
+    public function setCopyright(string $copyright): Channel
+    {
+        $this->copyright = $copyright;
+        return $this;
+    }
+
+    /**
+     * @param string $cover
+     * @return Channel
+     */
+    public function setCover(string $cover): Channel
+    {
+        $this->cover = htmlentities($cover);
+        return $this;
+    }
+
+    /**
+     * @param string $items
+     * @return Channel
+     */
+    public function setItems(string $items): Channel
+    {
+        $this->items = $items;
+        return $this;
+    }
+
+
+    public function __toString()
+    {
+        return <<<XML
+  <channel>
+   <title><![CDATA[$this->title]]></title>   
+   <link>$this->frontend</link>   
+   <language>$this->language</language>   
+   <description><![CDATA[$this->description<br>Feed: $this->feedUrl]]></description>   
+   <copyright><![CDATA[$this->copyright]]></copyright>   
+   <image>
+    <title><![CDATA[$this->title]]></title>
+    <url>$this->title</url>
+    <link>$this->frontend</link>
+   </image>
+   <itunes:image href="$this->cover"/>
+   <podcast:medium>video</podcast:medium>
+   $this->items
+  </channel>  
+XML;
+    }
+}
+
+class Item
+{
+    private string $title = '';
+    private string $episodeType = '';
+    private string $summary = '';
+    private string $uploaderUrl = '';
+    private string $description = '';
+    private string $thumbnail = '';
+    private string $duration = '';
+    private string $chaptersUrl = '';
+    private string $uploaderName = '';
+    private string $date;
+    private string $videoId;
+    private string $videoUrl;
+    private string $size;
+    private string $mimeType;
+    private string $url = '';
+
+    /**
+     * @param string $title
+     * @return Item
+     */
+    public function setTitle(string $title): Item
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @param string $episodeType
+     * @return Item
+     */
+    public function setEpisodeType(string $episodeType): Item
+    {
+        $this->episodeType = $episodeType;
+        return $this;
+    }
+
+    /**
+     * @param string $summary
+     * @return Item
+     */
+    public function setSummary(string $summary): Item
+    {
+        $this->summary = $summary;
+        return $this;
+    }
+
+    /**
+     * @param string $uploaderUrl
+     * @return Item
+     */
+    public function setUploaderUrl(string $uploaderUrl): Item
+    {
+        $this->uploaderUrl = htmlentities($uploaderUrl);
+        return $this;
+    }
+
+    /**
+     * @param string $description
+     * @return Item
+     */
+    public function setDescription(string $description): Item
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @param string $thumbnail
+     * @return Item
+     */
+    public function setThumbnail(string $thumbnail): Item
+    {
+        $this->thumbnail = htmlentities($thumbnail);
+        return $this;
+    }
+
+    /**
+     * @param string $duration
+     * @return Item
+     */
+    public function setDuration(string $duration): Item
+    {
+        $this->duration = $duration;
+        return $this;
+    }
+
+    /**
+     * @param string $chaptersUrl
+     * @return Item
+     */
+    public function setChaptersUrl(string $chaptersUrl): Item
+    {
+        $this->chaptersUrl = htmlentities($chaptersUrl);
+        return $this;
+    }
+
+    /**
+     * @param string $uploaderName
+     * @return Item
+     */
+    public function setUploaderName(string $uploaderName): Item
+    {
+        $this->uploaderName = htmlentities($uploaderName);
+        return $this;
+    }
+
+    /**
+     * @param string $date
+     * @return Item
+     */
+    public function setDate(string $date): Item
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    /**
+     * @param string $url
+     * @return Item
+     */
+    public function setUrl(string $url): Item
+    {
+        $this->url = htmlentities($url);
+        return $this;
+    }
+
+
+    /**
+     * @param string $videoId
+     * @return Item
+     */
+    public function setVideoId(string $videoId): Item
+    {
+        $this->videoId = $videoId;
+        return $this;
+    }
+
+    /**
+     * @param string $videoUrl
+     * @return Item
+     */
+    public function setVideoUrl(string $videoUrl): Item
+    {
+        $this->videoUrl = htmlentities($videoUrl);
+        return $this;
+    }
+
+    /**
+     * @param string $size
+     * @return Item
+     */
+    public function setSize(string $size): Item
+    {
+        $this->size = $size;
+        return $this;
+    }
+
+    /**
+     * @param string $mimeType
+     * @return Item
+     */
+    public function setMimeType(string $mimeType): Item
+    {
+        $this->mimeType = $mimeType;
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return <<<XML
+<item>
+    <title><![CDATA[$this->title]]></title>   
+    <itunes:episodeType>$this->episodeType</itunes:episodeType>
+    <itunes:summary><![CDATA[$this->summary]]></itunes:summary>  
+    <description><![CDATA[<center>$this->summary
+    <br><a href="$this->uploaderUrl">zum Kanal</a><br>＿＿＿＿＿＿＿＿＿＿＿＿＿＿<br><br></center>$this->description]]></description>  
+    <itunes:image href="$this->thumbnail"/> 
+    <itunes:duration>$this->duration</itunes:duration>
+    <podcast:chapters url="$this->chaptersUrl" type="application/json+chapters"/>
+    <podcast:person><![CDATA[$this->uploaderName]]></podcast:person>
+    <pubDate>$this->date</pubDate>
+    <link>$this->url</link>
+    <guid>$this->videoId</guid>
+    <enclosure url="$this->videoUrl" length="$this->size" type="$this->mimeType" />   
+</item>
+XML;
+    }
+}
+
 if (($_GET['clearcache'] ?? '') === '1' || ($_SERVER['HTTP_CACHE_CONTROL'] ?? '') === 'no-cache') {
     opcache_reset();
+    ini_set('display_errors', '1');
 }
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -11,61 +319,15 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if (strpos($path, '/thumb') === 0) {
     $videoId = $_GET['video'] ?? basename($path, '.jpg');
     $proxy = $_GET['proxy'] ?? 'pipedproxy.kavin.rocks';
-
-    $url = "https://$proxy/vi/$videoId/maxresdefault.jpg?host=i.ytimg.com";
-
-    $source_image = imagecreatefromwebp($url);
-
-    $source_imagex = imagesx($source_image);
-    $source_imagey = imagesy($source_image);
-
-    $dest_imagex = 1400;
-    $dest_imagey = intval($dest_imagex * ($source_imagey / $source_imagex));
-
-    $dest_image = imagecreatetruecolor(1400, 1400);
-    imagecopyresampled(
-        $dest_image,
-        $source_image,
-        0,
-        intval(700 - $dest_imagey / 2),
-        0,
-        0,
-        $dest_imagex,
-        $dest_imagey,
-        $source_imagex,
-        $source_imagey
-    );
-
-    header('Content-Type: image/jpeg');
-
-    imagejpeg($dest_image);
+    output_thumbnail($proxy, $videoId);
     exit;
 }
 
 $api = 'https://' . ($_GET['api'] ?? "pipedapi.kavin.rocks");
 
-
 if (strpos($path, '/chapters') === 0) {
     $videoId = $_GET['video'] ?? basename($path, '.json');
-
-    $streamUrl = "/streams/$videoId";
-
-
-    $streamData = fetch($api . $streamUrl);
-    $chapters = [
-        'version' => "1.2.0",
-        'chapters' => array_map(
-            fn(array $chapter) => [
-                'title' => $chapter['title'],
-                'img' => $chapter['image'],
-                'startTime' => $chapter['start']
-            ],
-            $streamData['chapters']
-        ),
-    ];
-
-    header('content-type: application/json+chapters');
-    echo json_encode($chapters);
+    output_chapters($api, $videoId);
     exit;
 }
 
@@ -76,50 +338,30 @@ $frontend = 'https://' . ($_GET['frontend'] ?? "piped.kavin.rocks");
 $format = $_GET['format'] ?? 'MPEG_4';
 $quality = $_GET['quality'] ?? '720p';
 $limit = (int)($_GET['limit'] ?? 300);
-$defaultTitles = [
-    'all' => 'YouTube',
-    'shorts' => 'YouTube Shorts',
-    'subscriptions' => 'YouTube Abos',
-];
-$channelTitle = $_GET['title'] ?? $defaultTitles[$mode] ?? 'YouTube';
-$channelDescription = $_GET['description'] ?? 'YouTube RSS-Podcast from ' . $frontend;
-$channelCopyright = $_GET['copyright'] ?? '&copy; YouTube';
-$channelLanguage = $_GET['language'] ?? 'en';
-$channelCover = htmlentities($_GET['cover'] ?? url('/logo.jpg'));
 
 $feedUrl = "/feed?authToken=$authToken";
 if (!$authToken) {
     $feedUrl = "/feed/unauthenticated?channels=$channels";
 }
-date_default_timezone_set('UTC');
-header('content-type: application/xml');
 
-$videos = fetch($api . $feedUrl);
+const TITLES = [
+    'all' => 'YouTube',
+    'shorts' => 'YouTube Shorts',
+    'subscriptions' => 'YouTube Abos',
+];
 
-$items = fetch_items($videos, $limit, $api, $format, $quality, $frontend, $mode);
+$channel = new Channel();
+$channel->setLanguage($_GET['language'] ?? 'en');
+$channel->setTitle($_GET['title'] ?? TITLES[$mode] ?? 'YouTube');
+$channel->setDescription($_GET['description'] ?? 'YouTube RSS-Podcast from ' . $frontend);
+$channel->setCopyright($_GET['copyright'] ?? '&copy; YouTube');
+$channel->setCover($_GET['cover'] ?? url('/logo.jpg'));
+$channel->setFrontend($frontend);
+$channel->setFeedUrl($api . $feedUrl);
+$channel->setItems(fetch_items(fetch($api . $feedUrl), $limit, $api, $format, $quality, $frontend, $mode));
 
-$xml = <<<XML
-<?xml version="1.0" encoding="UTF-8" ?>
- <rss version="2.0" xmlns:podcast="https://podcastindex.org/namespace/1.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:content="http://purl.org/rss/1.0/modules/content/">
-  <channel>
-   <title><![CDATA[$channelTitle]]></title>   
-   <link>$frontend</link>   
-   <language>$channelLanguage</language>   
-   <description><![CDATA[$channelDescription<br>Feed: $api$feedUrl]]></description>   
-   <copyright><![CDATA[$channelCopyright]]></copyright>   
-   <image>
-    <title><![CDATA[$channelTitle]]></title>
-    <url>$channelCover</url>
-    <link>$frontend</link>
-   </image>
-   <itunes:image href="$channelCover"/>
-   <podcast:medium>video</podcast:medium>
-   $items
-  </channel>   
- </rss>
-XML;
-
-echo $xml;
+output_feed($channel);
+exit;
 
 function fetch_items(
     array $videos,
@@ -147,32 +389,15 @@ function fetch_items(
                 }
                 $streamUrl = "/streams/$videoId";
                 $isShort = (bool)$video['isShort'];
-
                 if ($isShort && $mode !== 'shorts' || $mode === 'shorts' && !$isShort) {
                     continue;
                 }
-
                 $streamData = fetch($api . $streamUrl);
                 $fileInfo = find_video_file($streamData, $format, $quality);
                 if (empty($fileInfo)) {
                     continue;
                 }
                 $videoIds[$videoId] = true;
-                $mimeType = $fileInfo['mimeType'] ?? 'video/mp4';
-                $videoUrl = htmlentities($fileInfo['url'] ?? '');
-                $bitrate = (int)$fileInfo['bitrate'];
-                $duration = (int)$video['duration'];
-                $uploaderName = htmlentities($video['uploaderName']);
-                $description = $streamData['description'];
-                $views = format_count($streamData['views']);
-                $likes = format_count($streamData['likes']);
-                $uploaderSubscriberCount = format_count($streamData['uploaderSubscriberCount']);
-
-                $uploaderUrl = htmlentities(url($video['uploaderUrl'], $frontend));
-                $size = intval($duration * $bitrate / 8);
-                $summary = "👤$uploaderName<br>$uploaderSubscriberCount&nbsp;Abos | $views&nbsp;Aufr.&nbsp; | $likes&nbsp;Likes";
-
-                $title = $video['title'];
                 if ($isShort) {
                     $episodeType = 'trailer';
                 } elseif ($related) {
@@ -181,28 +406,32 @@ function fetch_items(
                     $episodeType = 'full';
                 }
 
-                $url = htmlentities($frontend . $video['url']);
-                $thumbnail = htmlentities(url("/thumb/$videoId.jpg"));
-                $date = date(DATE_RFC2822, intval($video['uploaded'] / 1000));
+                $uploaderName = $video['uploaderName'];
+                $views = format_count($streamData['views']);
+                $likes = format_count($streamData['likes']);
+                $subscribers = format_count($streamData['uploaderSubscriberCount']);
 
-                $chapters = htmlentities(url("/chapters/$videoId.json"));
+                $item = new Item();
+                $item->setTitle($video['title']);
+                $item->setEpisodeType($episodeType);
+                $item->setSummary(
+                    "👤$uploaderName<br>$subscribers&nbsp;Abos | $views&nbsp;Aufr.&nbsp; | $likes&nbsp;Likes"
+                );
+                $item->setUploaderUrl(url($video['uploaderUrl'], $frontend));
+                $item->setDescription($streamData['description']);
+                $item->setThumbnail(url("/thumb/$videoId.jpg"));
+                $item->setDuration((string)(int)$video['duration']);
+                $item->setChaptersUrl(url("/chapters/$videoId.json"));
+                $item->setUploaderName($uploaderName);
+                $item->setDate(date(DATE_RFC2822, intval($video['uploaded'] / 1000)));
+                $item->setUrl($frontend . $video['url']);
+                $item->setVideoUrl($fileInfo['url']);
+                $item->setVideoId($videoId);
+                $item->setSize((string)intval((int)$video['duration'] * (int)$fileInfo['bitrate'] / 8));
+                $item->setMimeType($fileInfo['mimeType'] ?? 'video/mp4');
 
-                $items .= <<<XML
-<item>
-    <title><![CDATA[$title]]></title>   
-    <itunes:episodeType>$episodeType</itunes:episodeType>
-    <itunes:summary><![CDATA[$summary]]></itunes:summary>  
-    <description><![CDATA[<center>$summary<br><a href="$uploaderUrl">zum Kanal</a><br>＿＿＿＿＿＿＿＿＿＿＿＿＿＿<br><br></center>$description]]></description>  
-    <itunes:image href="$thumbnail"/> 
-    <itunes:duration>$duration</itunes:duration>
-    <podcast:chapters url="$chapters" type="application/json+chapters"/>
-    <podcast:person><![CDATA[$uploaderName]]></podcast:person>
-    <pubDate>$date</pubDate>
-    <link>$url</link>
-    <guid>$videoId</guid>
-    <enclosure url="$videoUrl" length="$size" type="$mimeType" />   
-</item>
-XML;
+                $items .= $item;
+
                 if (!$related && $mode !== 'subscriptions' && isset($streamData['relatedStreams']) && is_array(
                         $streamData['relatedStreams']
                     )) {
@@ -283,3 +512,84 @@ function format_count($value): string
         return number_format(round($value, 1), 0, ',', '.');
     }
 }
+
+function output_chapters(string $api, string $videoId)
+{
+    $streamUrl = "/streams/$videoId";
+
+    $streamData = fetch($api . $streamUrl);
+    $chapters = [
+        'version' => "1.2.0",
+        'chapters' => array_map(
+            fn(array $chapter) => [
+                'title' => $chapter['title'],
+                'img' => $chapter['image'],
+                'startTime' => $chapter['start']
+            ],
+            $streamData['chapters']
+        ),
+    ];
+
+    header('content-type: application/json+chapters');
+    echo json_encode($chapters);
+}
+
+function output_thumbnail(string $proxy, string $videoId)
+{
+    $url = "https://$proxy/vi/$videoId/maxresdefault.jpg?host=i.ytimg.com";
+    if (!resource_exists($url)) {
+        $url = "https://$proxy/vi/$videoId/hqdefault.jpg?host=i.ytimg.com";
+    }
+
+    $source_image = imagecreatefromwebp($url);
+
+    $source_imagex = imagesx($source_image);
+    $source_imagey = imagesy($source_image);
+
+    $dest_imagex = 1400;
+    $dest_imagey = intval($dest_imagex * ($source_imagey / $source_imagex));
+
+    $dest_image = imagecreatetruecolor(1400, 1400);
+    imagecopyresampled(
+        $dest_image,
+        $source_image,
+        0,
+        intval(700 - $dest_imagey / 2),
+        0,
+        0,
+        $dest_imagex,
+        $dest_imagey,
+        $source_imagex,
+        $source_imagey
+    );
+
+    header('Content-Type: image/jpeg');
+
+    imagejpeg($dest_image);
+}
+
+function output_feed(Channel $channel)
+{
+    header('content-type: application/xml');
+    echo <<<XML
+<?xml version="1.0" encoding="UTF-8" ?>
+ <rss version="2.0" 
+ xmlns:podcast="https://podcastindex.org/namespace/1.0" 
+ xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" 
+ xmlns:content="http://purl.org/rss/1.0/modules/content/">
+  $channel   
+ </rss>
+XML;
+}
+
+function resource_exists(string $url): bool
+{
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+        CURLOPT_NOBODY => true,
+        CURLOPT_URL => $url,
+        CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'],
+    ]);
+    return curl_exec($ch) && 200 == curl_getinfo($ch, CURLINFO_HTTP_CODE);
+}
+
