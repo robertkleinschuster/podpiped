db7895ac8ff5c46731a7b572d5eb71414889957f
robertkleinschuster <git@robs.social>
2024-02-10 23:01:36 +0100

    feed cache

---
+++ status.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+header('content-type: text/plan');
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+http_response_code(200);
+flush();
+
+$locked = glob(__DIR__ . '/static/*.lock');
+$downloads = glob(__DIR__ . '/static/*.download');
+$channels = glob(__DIR__ . '/channel/*');
+
+echo "channels: " . count($channels);
+echo "\n";
+echo "downloads: " . count($downloads);
+echo "\n";
+echo "locked: " . count($locked);
