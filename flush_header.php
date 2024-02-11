742f70ffc5f4e121fb6b37f3a773a738d8394fce
robertkleinschuster <git@robs.social>
2024-02-11 13:19:53 +0100

    feed cache

--- flush_header.php
+++ flush_header.php
@@ -6,6 +6,8 @@ header('content-type: text/plan; charset=utf-8');
 header('Cache-Control: no-cache, must-revalidate, max-age=0');
 header('Pragma: no-cache');
 header("Content-Encoding: identity");
+header('Surrogate-Control: BigPipe/1.0');
+header('X-Accel-Buffering: no');
 header('Set-Cookie: rid=' . uniqid());
 if (function_exists('apache_setenv')) {
     apache_setenv('no-gzip', '1');
