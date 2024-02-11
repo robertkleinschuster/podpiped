614c4a48942ef2e585e55b0a55cf76f1d38df036
robertkleinschuster <git@robs.social>
2024-02-11 13:30:28 +0100

    feed cache

--- status.php
+++ status.php
@@ -10,13 +10,22 @@ $locked = array_map(fn($name) => basename($name, '.lock'), glob(__DIR__ . '/stat
 $downloads = array_map(fn($name) => basename($name, '.download'), glob(__DIR__ . '/static/*.download'));
 $cached = array_map(fn($name) => basename($name, '.mp4'), glob(__DIR__ . '/static/*.mp4'));
 
+flush();
+
 echo "channels: " . count(array_unique($channels));
 echo "\n";
+flush();
 echo "new channels: " . count($newChannels);
 echo "\n";
+flush();
 echo "videos: " . count(array_diff($cached, $locked));
 echo "\n";
+flush();
 echo "download queue: " . count(array_diff($downloads, $locked));
 echo "\n";
+flush();
 echo "download in progress: " . count($locked);
 echo "\n";
+flush();
+
+exit;
