e7fb8d249b824f20761836eeeff65eabcaada24a
robertkleinschuster <git@robs.social>
2024-02-11 14:46:34 +0100

    feed cache

--- status.php
+++ status.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 $channels =  array_map(fn($name) => basename($name, '.new'), glob(__DIR__ . '/channel/*'));
 $newChannels = array_map(fn($name) => basename($name, '.new'), glob(__DIR__ . '/channel/*.new'));
+$playlists =  array_map(fn($name) => basename($name, '.new'), glob(__DIR__ . '/playlist/*'));
+$newPlaylists = array_map(fn($name) => basename($name, '.new'), glob(__DIR__ . '/playlist/*.new'));
 $locked = array_map(fn($name) => basename($name, '.lock'), glob(__DIR__ . '/static/*.lock'));
 $downloads = array_map(fn($name) => basename($name, '.download'), glob(__DIR__ . '/static/*.download'));
 $cached = array_map(fn($name) => basename($name, '.mp4'), glob(__DIR__ . '/static/*.mp4'));
@@ -11,6 +13,10 @@ $cached = array_map(fn($name) => basename($name, '.mp4'), glob(__DIR__ . '/stati
 $result = "channels: " . count(array_unique($channels));
 $result .= "\n";
 $result .= "new channels: " . count($newChannels);
+$result .= "\n";
+$result = "playlists: " . count(array_unique($playlists));
+$result .= "\n";
+$result .= "new playlists: " . count($newPlaylists);
 $result .= "\n";
 $result .= "videos: " . count(array_diff($cached, $locked));
 $result .= "\n";
