b74f3db3a940c4aed7a2e2eac753d16658c4e205
robertkleinschuster <git@robs.social>
2024-02-11 10:05:06 +0100

    feed cache

--- status.php
+++ status.php
@@ -8,12 +8,15 @@ header('Pragma: no-cache');
 http_response_code(200);
 flush();
 
-$channels = glob(__DIR__ . '/channel/*');
+$channels =  array_map(fn($name) => basename($name, '.new'), glob(__DIR__ . '/channel/*'));
+$newChannels = array_map(fn($name) => basename($name, '.new'), glob(__DIR__ . '/channel/*.new'));
 $locked = array_map(fn($name) => basename($name, '.lock'), glob(__DIR__ . '/static/*.lock'));
 $downloads = array_map(fn($name) => basename($name, '.download'), glob(__DIR__ . '/static/*.download'));
 $cached = array_map(fn($name) => basename($name, '.mp4'), glob(__DIR__ . '/static/*.mp4'));
 
-echo "channels: " . count($channels);
+echo "channels: " . count(array_unique($channels));
+echo "\n";
+echo "new channels: " . count($newChannels);
 echo "\n";
 echo "videos: " . count(array_diff($cached, $locked));
 echo "\n";
