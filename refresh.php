0909576ea705fa9e6b458c78ff1557fe3cba7e9e
robertkleinschuster <git@robs.social>
2024-02-11 15:40:57 +0100

    feed cache

--- refresh.php
+++ refresh.php
@@ -12,15 +12,16 @@ $client = new Client($_SERVER['HTTP_HOST']);
 $cachedClient = new CachedClient($client);
 
 $time = time();
+
 do {
-    $complete = $cachedClient->refreshChannels();
+    $complete = $cachedClient->refreshPlaylists();
     flush();
     $status = require "status.php";
     file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'status', $status);
 } while (!$complete && time() - $time < 30);
 
 do {
-    $complete = $cachedClient->refreshPlaylists();
+    $complete = $cachedClient->refreshChannels();
     flush();
     $status = require "status.php";
     file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'status', $status);
