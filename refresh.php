f1530810439a6581fa96e1ee0cd3238ab948067d
robertkleinschuster <robert.kleinschuster@niceshops.com>
2024-02-12 11:31:32 +0100

    rerun refresh

--- refresh.php
+++ refresh.php
@@ -6,23 +6,35 @@ require_once "flush_header.php";
 require_once "Client.class.php";
 require_once "CachedClient.class.php";
 
+$start = $_GET['start'] ?? time();
 echo "\nrefreshing cached channels/playlists\n";
 
 $client = new Client($_SERVER['HTTP_HOST']);
 $cachedClient = new CachedClient($client);
 
-$time = time();
+$cachedClient->refreshPlaylists();
+$status = require "status.php";
+file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'status', $status);
 
-do {
-    $complete = $cachedClient->refreshPlaylists();
-    $status = require "status.php";
-    file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'status', $status);
-} while (!$complete && time() - $time < 30);
+$cachedClient->refreshChannels();
+$status = require "status.php";
+file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'status', $status);
 
-do {
-    $complete = $cachedClient->refreshChannels();
-    $status = require "status.php";
-    file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'status', $status);
-} while (!$complete && time() - $time < 30);
-
+if (time() - $start < 150) {
+    sleep(10);
+    try {
+        $url = "https://$_SERVER[HTTP_HOST]/refresh.php?start=" . $start;
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_URL => $url,
+            CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'],
+        ]);
+        curl_exec($ch);
+        curl_close($ch);
+        $log = new Log();
+        $log->append('rerun: ' . $url);
+    } catch (Throwable $exception) {
+        error_log((string)$exception);
+    }
+}
 exit;
