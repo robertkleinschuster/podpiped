235fd68088bcc1c869e34c03ff79c6f3822c0fa4
robertkleinschuster <robert.kleinschuster@niceshops.com>
2024-02-12 11:04:38 +0100

    increase lock wait time

--- flush_header.php
+++ flush_header.php
@@ -28,3 +28,18 @@ if (function_exists('fastcgi_finish_request')) {
 
 $status = require "status.php";
 file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'status', $status);
+try {
+    $run = $_GET['run'] ?? 1;
+    if ($run <= 2) {
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_URL => "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]?run=" . $run + 1,
+            CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'],
+        ]);
+
+        curl_exec($ch);
+        curl_close($ch);
+    }
+} catch (Throwable $exception) {
+    error_log((string)$exception);
+}
