def453c942c7b7a628dd5461381378982f093587
robertkleinschuster <github@robs.social>
2024-07-31 00:26:56 +0200

    convert more often

--- convert.php
+++ convert.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 require_once "flush_header.php";
 require_once "ImageConverter.class.php";
 
+$start = $_GET['start'] ?? time();
 echo "\nconverting images\n";
 
 $converter = new ImageConverter();
@@ -12,4 +13,22 @@ $converter->convert();
 
 $status = require "status.php";
 file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'status', $status);
+
+sleep(10);
+if (time() - $start < 250) {
+    try {
+        $url = "https://$_SERVER[HTTP_HOST]/convert.php?start=" . $start;
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_URL => $url,
+            CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'],
+        ]);
+        curl_exec($ch);
+        curl_close($ch);
+        $log = new Log();
+        $log->append('rerun: ' . $url);
+    } catch (Throwable $exception) {
+        error_log((string)$exception);
+    }
+}
 exit;
