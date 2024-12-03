ee80e8dbaf64145291c09eb0e17af8227cdc1ecb
robertkleinschuster <github@robs.social>
2024-12-03 20:30:00 +0100

    subscribe to channels in tubearchivist

--- Client.class.php
+++ Client.class.php
@@ -10,30 +10,6 @@ require_once "Rss.class.php";
 require_once "Log.class.php";
 require_once "Path.class.php";
 
-function unparse_url($parsed_url) {
-
-    $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
-
-    $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
-
-    $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-
-    $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
-
-    $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
-
-    $pass     = ($user || $pass) ? "$pass@" : '';
-
-    $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
-
-    $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
-
-    $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
-
-    return "$scheme$user$pass$host$port$path$query$fragment";
-
-}
-
 class Client
 {
     private Log $log;
@@ -158,9 +134,9 @@ class Client
             sprintf('Authorization: Token %s', file_get_contents(__DIR__ . '/../tubearchivist_token'))
 
         ]);
-        curl_exec($ch);
+        $response = curl_exec($ch);
         curl_close($ch);
-        $this->log->append("subscribed to channel in tubearchivist: " . $channelId);
+        $this->log->append("subscribed to channel in tubearchivist: " . $response);
     }
 
     public function channel(string $channelId, int $limit, bool $downloadVideos, int $downloadLimit, bool $downloadHq): ?Channel
